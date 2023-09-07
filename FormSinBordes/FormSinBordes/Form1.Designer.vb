433bfb0e03d645f5477e6300adc1739ce9709d2a
StringCGE <45083578+StringCGE@users.noreply.github.com>
2023-09-07 09:21:46 -0500

    Primer y posible ultima subida
    Es para mostrar como he solucionado hasta ahora el tema de form sin bordes ya que a microsoft no se le ocurrio un control para facilitarnos la vida

---
+++ FormSinBordes/FormSinBordes/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        OFD_ = New OpenFileDialog()
+        Panel1 = New Panel()
+        Label1 = New Label()
+        PNL_AreaCliente = New Panel()
+        PNL_Transparente = New Panel()
+        BTN_Cerrar = New Button()
+        BTN_MinMax = New Button()
+        BTN_Ocultar = New Button()
+        PNL_AreaCliente.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Yellow
+        Panel1.Location = New Point(6, 61)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(144, 37)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Yellow
+        Label1.Location = New Point(6, 122)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(317, 45)
+        Label1.TabIndex = 1
+        Label1.Text = "Abajo derecha si quieres cerrar" & vbCrLf & "En lo amarillo y transparente si quieres mover el formualrio" & vbCrLf & "A los laterales si quiereres rediensionar"
+        ' 
+        ' PNL_AreaCliente
+        ' 
+        PNL_AreaCliente.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        PNL_AreaCliente.BackColor = SystemColors.Control
+        PNL_AreaCliente.BorderStyle = BorderStyle.FixedSingle
+        PNL_AreaCliente.Controls.Add(PNL_Transparente)
+        PNL_AreaCliente.Controls.Add(BTN_Cerrar)
+        PNL_AreaCliente.Controls.Add(BTN_MinMax)
+        PNL_AreaCliente.Controls.Add(BTN_Ocultar)
+        PNL_AreaCliente.Controls.Add(Panel1)
+        PNL_AreaCliente.Controls.Add(Label1)
+        PNL_AreaCliente.Location = New Point(5, 5)
+        PNL_AreaCliente.Margin = New Padding(0)
+        PNL_AreaCliente.Name = "PNL_AreaCliente"
+        PNL_AreaCliente.Size = New Size(390, 390)
+        PNL_AreaCliente.TabIndex = 2
+        ' 
+        ' PNL_Transparente
+        ' 
+        PNL_Transparente.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        PNL_Transparente.BackColor = Color.Blue
+        PNL_Transparente.BorderStyle = BorderStyle.FixedSingle
+        PNL_Transparente.Location = New Point(6, 6)
+        PNL_Transparente.Name = "PNL_Transparente"
+        PNL_Transparente.Size = New Size(376, 49)
+        PNL_Transparente.TabIndex = 5
+        ' 
+        ' BTN_Cerrar
+        ' 
+        BTN_Cerrar.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        BTN_Cerrar.BackColor = Color.Silver
+        BTN_Cerrar.FlatAppearance.BorderSize = 0
+        BTN_Cerrar.FlatStyle = FlatStyle.Flat
+        BTN_Cerrar.Location = New Point(359, 359)
+        BTN_Cerrar.Margin = New Padding(0)
+        BTN_Cerrar.Name = "BTN_Cerrar"
+        BTN_Cerrar.Size = New Size(23, 23)
+        BTN_Cerrar.TabIndex = 4
+        BTN_Cerrar.Text = "X"
+        BTN_Cerrar.UseVisualStyleBackColor = False
+        ' 
+        ' BTN_MinMax
+        ' 
+        BTN_MinMax.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        BTN_MinMax.BackColor = Color.Silver
+        BTN_MinMax.FlatAppearance.BorderSize = 0
+        BTN_MinMax.FlatStyle = FlatStyle.Flat
+        BTN_MinMax.Location = New Point(335, 359)
+        BTN_MinMax.Margin = New Padding(0)
+        BTN_MinMax.Name = "BTN_MinMax"
+        BTN_MinMax.Size = New Size(23, 23)
+        BTN_MinMax.TabIndex = 3
+        BTN_MinMax.Text = "■"
+        BTN_MinMax.UseVisualStyleBackColor = False
+        ' 
+        ' BTN_Ocultar
+        ' 
+        BTN_Ocultar.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        BTN_Ocultar.BackColor = Color.Silver
+        BTN_Ocultar.FlatAppearance.BorderSize = 0
+        BTN_Ocultar.FlatStyle = FlatStyle.Flat
+        BTN_Ocultar.Location = New Point(311, 359)
+        BTN_Ocultar.Margin = New Padding(0)
+        BTN_Ocultar.Name = "BTN_Ocultar"
+        BTN_Ocultar.Size = New Size(23, 23)
+        BTN_Ocultar.TabIndex = 2
+        BTN_Ocultar.Text = "_"
+        BTN_Ocultar.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Blue
+        ClientSize = New Size(400, 400)
+        Controls.Add(PNL_AreaCliente)
+        DoubleBuffered = True
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        Text = "CGE"
+        TransparencyKey = Color.Blue
+        PNL_AreaCliente.ResumeLayout(False)
+        PNL_AreaCliente.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents OFD_ As OpenFileDialog
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PNL_AreaCliente As Panel
+    Friend WithEvents BTN_Cerrar As Button
+    Friend WithEvents BTN_MinMax As Button
+    Friend WithEvents BTN_Ocultar As Button
+    Friend WithEvents PNL_Transparente As Panel
+End Class
---
+++ FormSinBordes/FormSinBordes/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Drawing.IconLib
+Imports System.IO
+Imports System.Runtime.InteropServices
+'Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim miBorde As New Padding(5)
+    Const BORDE_SIZE As Integer = 10
+    Public Sub New()
+        InitializeComponent()
+        miBorde = New Padding(BORDE_SIZE)
+        FormBorderStyle = FormBorderStyle.None
+        BackColor = Color.Blue
+        TransparencyKey = Color.Blue
+        DoubleBuffered = True
+        AplicaRestaurarBorde()
+    End Sub
+
+    Protected Sub AplicaRestaurarBorde()
+        PNL_AreaCliente.SetBounds(miBorde.Left, miBorde.Top, Me.Width - miBorde.Left - miBorde.Right, Me.Height - miBorde.Top - miBorde.Bottom)
+    End Sub
+    Protected Sub AplicaMaximizarBorde()
+        PNL_AreaCliente.SetBounds(0, 0, Me.Width, Me.Height)
+    End Sub
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        MyBase.WndProc(m)
+        If m.Msg <> 132 Then ' OrElse m.Result.ToInt32() <> 1 Then
+            Return
+        End If
+        Dim screenPoint As New Point(m.LParam.ToInt32())
+        Dim clientPoint As Point = PointToClient(screenPoint)
+
+
+        Dim GetId As Func(Of Integer, Integer, Integer, IntPtr) = Function(A, B, C)
+                                                                      If clientPoint.X <= BORDE_SIZE Then
+                                                                          Return New IntPtr(A)
+                                                                      ElseIf clientPoint.X < (Size.Width - BORDE_SIZE) Then
+                                                                          Return New IntPtr(B)
+                                                                      Else
+                                                                          Return New IntPtr(C)
+                                                                      End If
+                                                                  End Function
+        'Lo curioso es quien hizo este codigo dejo los numeros en torno a cada posicion desde las cuales
+        'se puede redimensionar
+        If clientPoint.Y <= BORDE_SIZE Then
+            m.Result = GetId(13, 12, 14)
+        ElseIf clientPoint.Y <= (Size.Height - BORDE_SIZE) Then
+            m.Result = GetId(10, 0, 11) ' m.Result = GetId(10, 2, 11) el 2 es para mover el formulario desde la propia interaccion con el formulario
+        Else
+            m.Result = GetId(16, 15, 17)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseDown, Label1.MouseDown, MyBase.MouseDown, PNL_Transparente.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
+
+    Private Sub BTN_Cerrar_Click(sender As Object, e As EventArgs) Handles BTN_Cerrar.Click
+        Me.Close()
+        'Application.ExitThread()
+    End Sub
+
+    Dim EstaMaximizado As Boolean = False
+    Dim form_fws As FormWindowState
+    Dim form_size As Size
+    Dim form_loc As Point
+    Private Sub BTN_MinMax_Click(sender As Object, e As EventArgs) Handles BTN_MinMax.Click
+        EstaMaximizado = Not EstaMaximizado
+        If EstaMaximizado Then
+            AplicaMaximizarBorde()
+            form_fws = Me.WindowState
+            form_size = Me.Size
+            form_loc = Me.Location
+            'Me.WindowState = FormWindowState.Maximized
+
+            Dim mousePos As Point = Control.MousePosition
+            Dim screen As Screen = Screen.FromPoint(mousePos)
+            Dim screenName As String = screen.DeviceName
+            Dim screenWidth As Integer = screen.Bounds.Width
+            Dim screenHeight As Integer = screen.Bounds.Height
+            ' Obtener la pantalla en la que se encuentra el puntero del mouse
+            Dim currentScreen As Screen = Screen.FromPoint(Cursor.Position)
+
+            ' Obtener el tamaño del área de trabajo de la pantalla actual (excluyendo la barra de tareas)
+            Dim workingArea As Rectangle = currentScreen.WorkingArea
+            Me.Location = workingArea.Location
+            Me.Size = workingArea.Size
+        Else
+            AplicaRestaurarBorde()
+            Me.WindowState = form_fws
+            Me.Size = form_size
+            Me.Location = form_loc
+        End If
+    End Sub
+
+    Private Sub BTN_Ocultar_Click(sender As Object, e As EventArgs) Handles BTN_Ocultar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+#Region "Imports dll"
+    Public Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Public Const HT_CAPTION As Integer = &H2
+
+    <DllImportAttribute("user32.dll")>
+    Public Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+    End Function
+
+    <DllImportAttribute("user32.dll")>
+    Public Shared Function ReleaseCapture() As Boolean
+    End Function
+#End Region
+End Class
